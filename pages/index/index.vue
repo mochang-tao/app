4da20e5175ab541ef5dc40617cb4a5d6fffefe53
莫昌涛 <1962074554@qq.com>
2022-10-19 18:36:47 +0800

    初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="container">
+    <view>手机号：{{fromData.phone}}</view>
+    <view>密码：{{fromData.code}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+	  setup(props,context){
+		let fromData = {
+			phone:'18218877158',
+			code:'20020'
+		}
+		return { fromData }
+	  }
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
